79046ea6ea06086aa27b69893122ea16fdb17e01
julianatonu <julianatonu@gmail.com>
2025-07-21 23:03:40 +0600

    add

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,32 @@
 import Link from "next/link";
-import { stripe } from "./lib/stripe"
+import stripe from "./lib/stripe"
+import Image from "next/image";
 
 export default async function Home() {
   const products = await stripe.products.list({
     expand: ["data.default_price"],
     limit: 5,
   });
+
   console.log(products)
   return (
     <div>
-      <section>
-        <div>
-          <div>
-            <h2>Welcome to My E-commerce</h2>
-            <p>Discover the latest products at the best prices</p>
+      <section className="rounded bg-neutral-100 py-8 sm:py-12">
+        <div className="mx-auto grid grid-cols-2 items-center justify-items-center gap-8 py-8 sm:px-16">
+          <div className="max-w-md space-y-4">
+            <h2 className="text-3xl font-bold tracking-tight md;text-4xl">Welcome to My E-commerce</h2>
+            <p className="text-neutral-600">Discover the latest products at the best prices</p>
             <button >
               <Link href='/products'>Browse All products</Link>
             </button>
+            < Image
+              className="inline-flex items-center justify-center rounded-full px-6 py-3 bg-black "
+              src={products.data[2].images[0]}
+              alt="Banner Image"
+              width={450}
+              height={450}
+            />
+
           </div>
         </div>
       </section>
